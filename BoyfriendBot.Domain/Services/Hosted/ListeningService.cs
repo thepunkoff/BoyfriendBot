ffa47317145d3633bbc5c1fe4ce9d7c637e94228
thepunkoff <thepunkoff@gmail.com>
2020-03-19 18:23:25 +0300

    message details logging

--- BoyfriendBot.Domain/Services/Hosted/ListeningService.cs
+++ BoyfriendBot.Domain/Services/Hosted/ListeningService.cs
@@ -7,8 +7,6 @@ using BoyfriendBot.Domain.Services.Interfaces;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -74,15 +72,82 @@ namespace BoyfriendBot.Domain.Services.Hosted
 
         async void OnMessage(object sender, MessageEventArgs eventArgs)
         {
-            var userId = eventArgs.Message.From.Id;
+            var message = eventArgs.Message;
+            var userId = message.From.Id;
+
+            LogMessage(message);
 
             if (!_userStorage.HasUser(userId))
             {
-                var mappedUser = _mapper.Map<UserDbo>(eventArgs.Message);
+                var mappedUser = _mapper.Map<UserDbo>(message);
 
                 await _userStorage.AddNewUser(mappedUser);
             }
             await _botClient.SendTextMessageAsync(eventArgs.Message.Chat, "Прости, я тебя пока что не понимаю!");
+        }
+
+        private void LogMessage(Message message)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Incoming message ({message.MessageId}) from {message.From.FirstName} {message.From.LastName} ({message.From.Id})");
+
+            if (message.ReplyToMessage != null)
+            {
+                var mId = message.ReplyToMessage.MessageId;
+
+                var fn = message.ReplyToMessage.From.FirstName;
+                var ln = message.ReplyToMessage.From.LastName;
+                var id = message.ReplyToMessage.From.Id;
+
+                sb.AppendLine($"Replied to message ({mId}) from {fn} {ln} ({id}): \"{message.ReplyToMessage.Text}\"");
+            }
+
+            if (message.ForwardFrom != null)
+            {
+                var fn = message.ForwardFrom.FirstName;
+                var ln = message.ForwardFrom.LastName;
+                var id = message.ForwardFrom.Id;
+
+                sb.AppendLine($"Forwarded from {fn} {ln} ({id}): \"{message.Text}\"");
+            }
+
+            if (message.Text != null)
+            {
+                sb.AppendLine($"Text: \"{message.Text}\"");
+            }
+            if (message.Sticker != null)
+            {
+                sb.AppendLine($"Sticker: \"{message.Sticker.Emoji}\"");
+            }
+            if (message.Photo != null)
+            {
+                sb.AppendLine($"Picture recieved. Picture caption: \"{message.Caption}\"");
+            }
+            if (message.Audio != null)
+            {
+                sb.AppendLine($"Audio recieved. Duration: \"{message.Audio.Duration}\"");
+            }
+            if (message.Voice != null)
+            {
+                sb.AppendLine($"Voice recieved. Duration: \"{message.Voice.Duration}\"");
+            }
+            if (message.VideoNote != null)
+            {
+                sb.AppendLine($"Video note recieved. Duration: \"{message.VideoNote.Duration}\"");
+            }
+            if (message.Venue != null)
+            {
+                sb.AppendLine($"Audio recieved. Address: \"{message.Venue.Address}\"");
+            }
+            if (message.Document != null)
+            {
+                sb.AppendLine($"Document recieved. File name: \"{message.Document.FileName}\"");
+            }
+
+            var log = sb.ToString().TrimEnd('\n');
+
+            _logger.LogInformation(log);
         }
     }
 }
