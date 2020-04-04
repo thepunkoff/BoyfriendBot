0744aef5b5eec400f39f98c3421d50d612fbcecf
thepunkoff <thepunkoff@gmail.com>
2020-04-05 01:33:23 +0300

    Create create.sql

---
+++ BoyfriendBot.Domain/Data/Scripts/create.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE "User" ("ChatId" long primary key, "Username" text, "UserId" long, "FirstName" text, "LastName" text);
+
+
+CREATE TABLE "RarityWeights"
+(
+	"ChatId" long,
+	"WhiteWeight" int not null default 100,
+	"GreenWeight" int not null default 20,
+	"BlueWeight" int not null default 10,
+	"PurpleWeight" int not null default 5,
+	"OrangeWeight" int not null default 1,
+    FOREIGN KEY ("ChatId") REFERENCES "User"("ChatId")
+
+);
+
+
+CREATE TABLE "UserSettings"
+(
+	"ChatId" long,
+	"RecieveReminders" boolean not null default true,
+	"RecieveScheduled" boolean not null default true, "Gender" boolean not null default 0, "BotGender" boolean not null default 1,
+    FOREIGN KEY ("ChatId") REFERENCES "User"("ChatId")
+);
